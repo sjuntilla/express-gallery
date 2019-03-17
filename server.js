de8347a9c1a5c51d1edec9e818d5726f23ca1acf
justen <justen@justens-MBP.home>
2019-03-16 19:29:50 -1000

    id is shown in edit but doesn't actually edit the data

--- server.js
+++ server.js
@@ -1,38 +1,40 @@
 const knex = require("./database/index.js");
 const express = require("express");
 const app = express();
-const hbs = require('express-handlebars');
+const hbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 
-const Gallery = require('./models/gallerymodel.js');
-
+const Gallery = require("./models/gallerymodel.js");
 
 const PORT = process.env.PORT;
 if (!PORT) {
   console.log("Port not found!");
-};
+}
 
 // EXPRESS SERVER STUFF
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 app.use(bodyParser.json());
 
 //HBS STUFF
-app.engine('handlebars', hbs({
-  defaultLayout: 'index'
-}));
-app.set('view engine', 'handlebars');
-
-
+app.engine(
+  "handlebars",
+  hbs({
+    defaultLayout: "index"
+  })
+);
+app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
-  return new Gallery().fetchAll()
-    .then((gallerytable) => {
-      return res.render('main', {
+  return new Gallery()
+    .fetchAll()
+    .then(gallerytable => {
+      return res.render("main", {
         gallerytable
       });
-
     })
     .catch(err => {
       console.log(err);
@@ -41,43 +43,57 @@ app.get("/", (req, res) => {
 });
 
 app.get("/gallery", (req, res) => {
-  return new Gallery().fetchAll()
-    .then((gallerytable) => {
+  return new Gallery()
+    .fetchAll()
+    .then(gallerytable => {
       return res.json(gallerytable);
     })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
       res.sendStatus(500);
     });
 });
 //DISPLAYS A PAGE WITH FORM TO ADD AN IMAGE TO THE GALLERY
-app.get("/gallery/new", (req, res) => {});
+app.get("/gallery/new", (req, res) => {
+  return new Gallery()
+    .fetchAll()
+    .then(gallerytable => {
+      return res.render("new", {
+        gallerytable
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
 
 //ACTUALLY ADDS AN IMAGE TO THE GALLERY
 app.post("/gallery", (req, res) => {
-
   const body = req.body;
   return Gallery.forge({
     author: body.author,
     link: body.link,
-    description: body.description,
-  }).save(null, {
-    method: 'insert'
-  }).then(() => {
-    new Gallery({
-      link: body.link
-    }).fetch().then((img) => {
-      return res.json({
-        'eat': 'my entire ass'
-      });
+    description: body.description
+  })
+    .save(null, {
+      method: "insert"
     })
-
-  });
+    .then(() => {
+      new Gallery({
+        link: body.link
+      })
+        .fetch()
+        .then(img => {
+          return res.json({
+            eat: "my entire ass"
+          });
+        });
+    });
 });
 
 //RETRIEVES SPECIFIC IMAGE BY ID
 app.get("/gallery/:id", (req, res) => {
-
   let reqParams = req.params.id;
   return new Gallery()
     .where({
@@ -97,12 +113,14 @@ app.get("/gallery/:id", (req, res) => {
 app.get("/gallery/:id/edit", (req, res) => {
   let paramsId = req.params.id;
   return Gallery.where({
-      id: paramsId
-    })
-    .fetch().then((img) => {
-      return res.json({
-        'get': 'fucked'
-      })
+    id: paramsId
+  })
+    .fetch()
+    .then(img => {
+      console.log(img.attributes);
+      let id = img.attributes.id;
+      let galleryObj = img.attributes;
+      res.render("edit", galleryObj);
     });
 });
 
@@ -113,21 +131,28 @@ app.put("/gallery/:id", (req, res) => {
 
   Gallery.where({
     id: paramsId
-  }).fetch().then((img) => {
-    new Gallery({
-      id: paramsId
-    }).save({
-      link: body.link,
-      description: body.description,
-      author: body.author
-    }, {
-      patch: true
-    }).then(() => {
-      return res.json({
-        'i hate': 'bookshelf'
+  })
+    .fetch()
+    .then(img => {
+      new Gallery({
+        id: paramsId
       })
+        .save(
+          {
+            link: body.link,
+            description: body.description,
+            author: body.author
+          },
+          {
+            patch: true
+          }
+        )
+        .then(() => {
+          return res.json({
+            "i hate": "bookshelf"
+          });
+        });
     });
-  });
 });
 
 //DELETES AN IMAGE BY ID
@@ -136,13 +161,17 @@ app.delete("/gallery/:id", (req, res) => {
 
   Gallery.where({
     id: paramsId
-  }).fetch().then((img) => {
-    new Gallery({
-      id: paramsId
-    }).destroy().then(() => {
-      return res.redirect('/gallery');
+  })
+    .fetch()
+    .then(img => {
+      new Gallery({
+        id: paramsId
+      })
+        .destroy()
+        .then(() => {
+          return res.redirect("/gallery");
+        });
     });
-  });
 });
 
 app.listen(PORT, () => {
